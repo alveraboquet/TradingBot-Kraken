d374222568258da0aba084983dd1b7e005235cad
Mathieu Noyelle <37225914+ikuroNoriiwa@users.noreply.github.com>
2020-11-16 20:20:04 +0100

    :hammer_and_wrench: [Modif] ajout de fonction dans getMetrics

--- GetMetrics.py
+++ GetMetrics.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import time, datetime
+import os.path
+import crypto_list
 
 class GetAllMetrics:
 
@@ -11,24 +13,43 @@ class GetAllMetrics:
             js = json.loads(response.content.decode('utf-8'))
             return js['result'][currency]
 
+    def insert_header(self, currency):
+        file = open("crypto_infos/" + currency + ".csv", "w")
+        file.write("Date,High,Low,OpenPrice,Close,Average\n")
+        file.close()
+
     def format_information(self, currency):
-        file = open(currency + ".csv", "a")
+
         req = self.get_all_informations(currency)
         high = float(req['h'][1])
         low = float(req['l'][1])
-        market = float(req['o'])
-        moy = (float(high) + float(low) + float(market)) / 3
+        open_market = float(req['o'])
+        close_market = float(req['c'][0])
+
+        moy = (float(high) + float(low) + float(open_market)) / 3
         current_time = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
-        file.write("{0},{1:.2f},{2:.2f},{3:.2f},{4:.2f}\n".format(current_time,high,low, market, moy))
+
+        str = "{0},{1:.2f},{2:.2f},{3:.2f},{4:.2f},{5:.2f}\n".format(current_time,high,low, open_market, close_market, moy)
+
+
+        return str
+
+    def write_to_csv(self, filename, data):
+        if not os.path.isfile("crypto_infos/" + filename + ".csv"):
+            self.insert_header(filename)
+        file = open("crypto_infos/" + filename + ".csv", "a")
+        file.write(data)
         file.close()
 
 def main():
-    currency = ['XETHZEUR', 'XXBTZEUR']
+    currency = crypto_list.__cur__
     metrics = GetAllMetrics()
     starttime = time.time()
     while True:
         for cur in currency:
-            metrics.format_information(cur)
+            filename = "{}_{}".format(cur, currency[cur][1])
+            data = metrics.format_information(currency[cur][1])
+            metrics.write_to_csv(filename, data)
         time.sleep(60.0 - ((time.time() - starttime) % 60.0))
 
 if __name__ == '__main__':
